47c91cec4ae10a2d1b64b6ec6680d7dd2234b4e5
SkylArt0 <vm.moreno.2022@alumnos.urjc.es>
2025-05-28 20:15:27 +0200

    Avances1
    Hemos añadido una nueva escena de inicio para conectar los diferentes players, pero no está aún bien gestionado el movimiento, hay que hacerlo independiente en cada uno.

---
+++ EMZERO-main/Assets/Scripts/UIManager.cs
@@ -0,0 +1,45 @@
+using System;
+using Unity.Netcode;
+using UnityEngine;
+
+    public class UIManager : MonoBehaviour
+    {
+        [SerializeField]
+        NetworkManager m_NetworkManager;
+
+        void Awake()
+        {
+        }
+
+        void OnGUI()
+        {
+            GUILayout.BeginArea(new Rect(10, 10, 300, 300));
+            if (!m_NetworkManager.IsClient && !m_NetworkManager.IsServer)
+            {
+                StartButtons();
+            }
+            else
+            {
+                StatusLabels();
+            }
+
+            GUILayout.EndArea();
+        }
+
+        void StartButtons()
+        {
+            if (GUILayout.Button("Host")) m_NetworkManager.StartHost();
+            if (GUILayout.Button("Client")) m_NetworkManager.StartClient();
+            if (GUILayout.Button("Server")) m_NetworkManager.StartServer();
+        }
+
+        void StatusLabels()
+        {
+            var mode = m_NetworkManager.IsHost ?
+                "Host" : m_NetworkManager.IsServer ? "Server" : "Client";
+
+            GUILayout.Label("Transport: " +
+                m_NetworkManager.NetworkConfig.NetworkTransport.GetType().Name);
+            GUILayout.Label("Mode: " + mode);
+        }
+    }
